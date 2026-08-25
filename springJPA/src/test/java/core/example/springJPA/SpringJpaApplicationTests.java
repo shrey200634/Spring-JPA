babbdfe370fe374469e79fbbe2676eded73f7724
shrey200634 <dshrey60@gmail.com>
2026-08-25 15:50:53 +0530

    setup the mysql in jdbc

---
+++ springJPA/src/test/java/core/example/springJPA/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package core.example.springJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
